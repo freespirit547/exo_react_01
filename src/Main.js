23b709550c0c682b68651f26b6710b9a671c271b
Sokuntheavy Roeung <sokuntheavy.roeung@lapiscine.pro>
2023-11-07 11:43:03 +0100

    commit 1

---
+++ src/Main.js
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+       <main>
+            <h1>Hello La Piscine</h1>
+            <p>Je suis le paragraphe de main</p>
+       </main>
+    )
+   }
+   
+   export default Main;
