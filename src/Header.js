3a60f4caa90d6f2c5c7a3ebbc083c04ba3ca17ea
Sokuntheavy Roeung <sokuntheavy.roeung@lapiscine.pro>
2023-11-07 14:24:15 +0100

    commit 2

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+       <header>
+            <p>Je suis le header</p>
+            <title>Titre du header</title>
+            <ul>
+                <li>Menu el 1</li>
+                <li>Menu el 2</li>
+                <li>Menu el 3</li>
+            </ul>
+       </header>
+    )
+   }
+   
+   export default Header;
